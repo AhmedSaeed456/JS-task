744f6c518c3c1e5c28d6f9d010d755a42011576d
ahmed saeed <76794210+AhmedSaeed456@users.noreply.github.com>
2022-11-24 19:54:47 +0200

    Create script.js

---
+++ task1-textedit/script.js
@@ -0,0 +1,39 @@
+function ChangeFont(font){
+
+    document.getElementById("PAR").style.fontFamily = font;
+}
+
+function ChangeAlign(ali){
+    
+    document.getElementById("PAR").style.textAlign = ali;
+}
+
+function ChangeHeight(hi) {
+    document.getElementById("PAR").style.lineHeight=hi;
+}
+
+
+function ChangeLSpace(CS){
+    
+    document.getElementById("PAR").style.letterSpacing = CS;
+}
+
+function ChangeIndent(CI) {
+    document.getElementById("PAR").style.textIndent = CI;
+}
+
+function ChangeTransform(CF){
+    document.getElementById("PAR").style.textTransform = CF;
+}
+
+function ChangeDecorate(CD){
+    document.getElementById("PAR").style.textDecoration = CD;
+}
+
+function ChangeBorder(CB){
+    document.getElementById("PAR").style.borderStyle = CB;
+}
+
+function ChangeBorderColor(CBC){
+    document.getElementById("PAR").style.borderColor = CBC;
+}
